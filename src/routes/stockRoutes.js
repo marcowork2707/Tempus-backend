19a0290f973a4b300c60e74fbbfb3b7ffcd4baaa
marcowork2707 <marco.munoz@alu.uclm.es>
2026-04-13 16:53:56 +0200

    Enhance stock routes to allow access for 'encargado' and 'coach' roles in config and report endpoints

--- src/routes/stockRoutes.js
+++ src/routes/stockRoutes.js
@@ -11,13 +11,13 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 // Config routes
 router
   .route('/:centerId/config')
-  .get(isAuthenticatedUser, authorizeRoles('admin'), getStockConfig)
+  .get(isAuthenticatedUser, authorizeRoles('admin', 'encargado', 'coach'), getStockConfig)
   .put(isAuthenticatedUser, authorizeRoles('admin'), upsertStockConfig);
 
 // Report routes
 router
   .route('/:centerId/reports')
-  .get(isAuthenticatedUser, authorizeRoles('admin'), getStockReports)
-  .post(isAuthenticatedUser, authorizeRoles('admin'), submitStockReport);
+  .get(isAuthenticatedUser, authorizeRoles('admin', 'encargado', 'coach'), getStockReports)
+  .post(isAuthenticatedUser, authorizeRoles('admin', 'encargado', 'coach'), submitStockReport);
 
 module.exports = router;
